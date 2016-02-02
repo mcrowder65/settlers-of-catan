d50b23497e6641a003d17c6a47534fc5b6abaf8b
bbarney7 <brennen.barney@gmail.com>
2016-02-02 12:34:05 -0700

    updated facade testing

--- Project/src/shared/definitions/FacadeCanDoTest.java
+++ Project/src/shared/definitions/FacadeCanDoTest.java
@@ -50,5 +50,47 @@ public class FacadeCanDoTest {
 		canDiscard = facade.canDiscardCards();
 		assertTrue(canDiscard == false);
 	}
+	
+	@Test
+	public void canDiscardCardsTrue() {
+		
+		GameManager game = new GameManager();
+		Facade facade = new Facade(game);
+		GameModel gameModel = new GameModel();
+		Player player = new Player();
+		ResourceList resources = new ResourceList(1,2,3,2,1);
+		game.updateModel(gameModel);
+		
+		player.setPlayerID(0);
+		player.setResources(resources);
+		gameModel.setLocalPlayer(player);
+		
+		boolean canDiscard = facade.canDiscardCards();
+		assertTrue(canDiscard == true);
+		
+		canDiscard = true;
+		resources = new ResourceList(10,0,0,0,0);
+		player.setResources(resources);
+		canDiscard = facade.canDiscardCards();
+		assertTrue(canDiscard == true);
+		
+		canDiscard = true;
+		resources = new ResourceList(2,3,1,1,1);
+		player.setResources(resources);
+		canDiscard = facade.canDiscardCards();
+		assertTrue(canDiscard == true);
+		
+		canDiscard = true;
+		resources = new ResourceList(0,4,3,0,1);
+		player.setResources(resources);
+		canDiscard = facade.canDiscardCards();
+		assertTrue(canDiscard == true);
+		
+		canDiscard = true;
+		resources = new ResourceList(19,10,10,10,10);
+		player.setResources(resources);
+		canDiscard = facade.canDiscardCards();
+		assertTrue(canDiscard == true);
+	}
 
 }

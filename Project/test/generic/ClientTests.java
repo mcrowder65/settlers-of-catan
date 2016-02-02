f535ca691ac2bf3e4a7d0b8bb98b6ad7065254fd
Eric <Eric@Eric-PC2.app.byu.edu>
2016-02-01 21:59:48 -0700

    Added generic tests class

---
+++ Project/test/generic/ClientTests.java
@@ -0,0 +1,25 @@
+package generic;
+
+public class ClientTests {
+
+	
+	public static void main(String[] args) {
+		String[] testClasses = new String[] {
+				"communication.CommandTests",
+				"communication.HTTPProxyTest",
+				"communication.MockProxyTests",
+				"communication.PollerTests",
+				
+				"gameMapTest.deleteSettlementTest",
+				"gameMapTest.HasMunicipalityTest",
+				"gameMapTest.HasSettlementTest",
+				"gameMapTest.LayCityTest",
+				"gameMapTest.LaySettlementTest",
+				"gameMapTest.VertexObjectFactoryTest",
+				
+				"turnTracker.TurnTrackerTest"
+		};
+
+		org.junit.runner.JUnitCore.main(testClasses);
+	}
+}

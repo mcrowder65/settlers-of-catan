d76a551633bb8dd93129f4ca0a5aea52b1252882
Matt Crowder <mcrowder65@gmail.com>
2016-02-04 19:19:49 -0700

    testing map works

--- Project/test/client/utils/TranslatorTest.java
+++ Project/test/client/utils/TranslatorTest.java
@@ -51,7 +51,7 @@ public class TranslatorTest {
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-		//System.out.println(append.toString());
+		
 		Object temp = Translator.jsonToObject(append.toString());
 		String json = Translator.objectToJson(temp);
 		System.out.println(json);
@@ -79,6 +79,54 @@ public class TranslatorTest {
 		if(deck.equals(gameModel.getDeck()) == true) fail();
 		deck = new DevCardList(2, 5, 2, 14, 2);
 		if(deck.equals(gameModel.getDeck()) == false) fail();
+		
+		ResourceList bank = new ResourceList(24, 24, 24, 24, 24);
+		if(gameModel.getBank().equals(bank) == false) fail();
+		
+		
+		
+	}
+	public void makeHexes(Hex[] hexes){
+		//HexLocation location, ResourceType resource, int number
+		hexes[0] = new Hex(new HexLocation(0, -2), Translator.getResourceType("asdf"), -1);
+		hexes[1] = new Hex(new HexLocation(1, -2), Translator.getResourceType("brick"), 4);
+		hexes[2] = new Hex(new HexLocation(2, -2), Translator.getResourceType("wood"), 11);
+		
+		hexes[3] = new Hex(new HexLocation(-1, -1), Translator.getResourceType("brick"), 8);
+		
+		hexes[4] = new Hex(new HexLocation(0, -1), Translator.getResourceType("wood"), 3);
+		
+		hexes[5] = new Hex(new HexLocation(1, -1), Translator.getResourceType("ore"), 9);
+		
+		hexes[6] = new Hex(new HexLocation(2, -1), Translator.getResourceType("sheep"), 12);
+		
+		hexes[7] = new Hex(new HexLocation(-2, 0), Translator.getResourceType("ore"), 5);
+		
+		hexes[8] = new Hex(new HexLocation(-1, 0), Translator.getResourceType("sheep"), 10);
+		
+		hexes[9] = new Hex(new HexLocation(0, 0), Translator.getResourceType("wheat"), 11);
+		
+		hexes[10] = new Hex(new HexLocation(1, 0), Translator.getResourceType("brick"), 5);
+		
+		hexes[11] = new Hex(new HexLocation(2, 0), Translator.getResourceType("wheat"), 6);
+		
+		hexes[12] = new Hex(new HexLocation(-2, 1), Translator.getResourceType("wheat"), 2);
+		
+		hexes[13] = new Hex(new HexLocation(-1, 1), Translator.getResourceType("sheep"), 9);
+		
+		hexes[14] = new Hex(new HexLocation(0, 1), Translator.getResourceType("wood"), 4);
+		
+		hexes[15] = new Hex(new HexLocation(1, 1), Translator.getResourceType("sheep"), 10);
+		
+		hexes[16] = new Hex(new HexLocation(-2, 2), Translator.getResourceType("wood"), 6);
+		
+		hexes[17] = new Hex(new HexLocation(-1, 2), Translator.getResourceType("ore"), 3);
+		
+		hexes[18] = new Hex(new HexLocation(0, 2), Translator.getResourceType("wheat"), 8);
+		
+		
+	}
+	public void testMap(GameModel gameModel){
 		Port[] ports = new Port[9];
 		setPorts(ports);
 		for(int i = 0; i < ports.length; i++){
@@ -95,6 +143,17 @@ public class TranslatorTest {
 				continue;
 			if(players[i].equals(gameModel.getPlayers()[i]) == false)
 				fail();
+		}
+		if(gameModel.getMap().getRoads().length != 0) fail();
+		if(gameModel.getMap().getCities().length != 0) fail();
+		if(gameModel.getMap().getSettlements().length != 0) fail();
+		if(gameModel.getMap().getRadius() != 3) fail();
+		if(gameModel.getLog().size() != 0) fail();
+		if(gameModel.getChat().size() != 0) fail();
+		
+		Hex[] hexes = new Hex[19];
+		for(int i = 0; i < hexes.length; i++){
+			if(hexes[i].equals(gameModel.getMap().getHexes()[i]) == false) fail();
 		}
 	}
 	public void setPlayers(Player[] players){

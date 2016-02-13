43446307ae912371b635fd608677627ea73c5b55
bbarney7 <brennen.barney@gmail.com>
2016-02-13 15:12:38 -0700

    Implemented firstRound and secondRound states

--- Project/src/client/gamestate/FirstRoundState.java
+++ Project/src/client/gamestate/FirstRoundState.java
@@ -1,5 +1,22 @@
 package client.gamestate;
 
-public class FirstRoundState extends GameState {
+import client.controller.Facade;
+import shared.locations.EdgeLocation;
+import shared.locations.VertexLocation;
 
+public class FirstRoundState extends GameState {
+	
+	public FirstRoundState(){
+		
+	}
+	
+	@Override
+	public boolean canPlaceRoad(EdgeLocation edgeLoc, Facade facade) throws IllegalArgumentException{
+		return facade.canBuildRoad(edgeLoc);
+	}
+
+	@Override
+	public boolean canPlaceSettlement(VertexLocation vertLoc, Facade facade) throws IllegalArgumentException{
+		return facade.canBuildSettlement(vertLoc);
+	}
 }
--- Project/src/client/gamestate/SecondRoundState.java
+++ Project/src/client/gamestate/SecondRoundState.java
@@ -1,5 +1,22 @@
 package client.gamestate;
 
-public class SecondRoundState extends GameState {
+import client.controller.Facade;
+import shared.locations.EdgeLocation;
+import shared.locations.VertexLocation;
 
+public class SecondRoundState extends GameState {
+	
+	public SecondRoundState(){
+		
+	}
+	
+	@Override
+	public boolean canPlaceRoad(EdgeLocation edgeLoc, Facade facade) throws IllegalArgumentException{
+		return facade.canBuildRoad(edgeLoc);
+	}
+
+	@Override
+	public boolean canPlaceSettlement(VertexLocation vertLoc, Facade facade) throws IllegalArgumentException{
+		return facade.canBuildSettlement(vertLoc);
+	}
 }

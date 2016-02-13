87648da29046452a1c92b74e97bc178486e04619
bbarney7 <brennen.barney@gmail.com>
2016-02-13 14:23:49 -0700

    added a few methods to gamestate

--- Project/src/client/gamestate/GameState.java
+++ Project/src/client/gamestate/GameState.java
@@ -1,5 +1,10 @@
 package client.gamestate;
 
+import shared.definitions.CatanColor;
+import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
+
 public abstract class GameState {
 
 	public GameState identifyState(String state){
@@ -15,6 +20,42 @@ public abstract class GameState {
 		else if(state.equals("Second Round")) gameState = new SecondRoundState();
 		
 		return gameState;
+	}
+	
+	public boolean canPlaceRoad(EdgeLocation edgeLoc) {
+
+		return false;
+	}
+
+	public boolean canPlaceSettlement(VertexLocation vertLoc) {
+
+		return false;
+	}
+
+	public boolean canPlaceCity(VertexLocation vertLoc) {
+
+		return false;
+	}
+
+	public boolean canPlaceRobber(HexLocation hexLoc) {
+
+		return false;
+	}
+
+	public void placeRoad(EdgeLocation edgeLoc) {
+
+	}
+
+	public void placeSettlement(VertexLocation vertLoc) {
+
+	}
+
+	public void placeCity(VertexLocation vertLoc) {
+
+	}
+
+	public void placeRobber(HexLocation hexLoc) {
+
 	}
 	
 }

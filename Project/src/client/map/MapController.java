bc882252635228aa2a4b6935a1c5ef674b42161f
menyx02 <meny_marquez@hotmail.com>
2016-02-16 12:37:59 -0700

    a

--- Project/src/client/map/MapController.java
+++ Project/src/client/map/MapController.java
@@ -182,7 +182,7 @@ public class MapController extends Controller implements IMapController, Observe
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-
+	
 	}
 
 }

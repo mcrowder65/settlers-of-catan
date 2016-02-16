b2f522b7b95c900795d722556bbd2e3b4d6b8a4a
bbarney7 <brennen.barney@gmail.com>
2016-02-16 13:23:55 -0700

    UPDATES

--- Project/src/client/discard/DiscardController.java
+++ Project/src/client/discard/DiscardController.java
@@ -6,6 +6,7 @@ import java.util.Observable;
 import java.util.Observer;
 
 import client.base.*;
+import client.controller.Facade;
 import client.data.GameManager;
 import client.gamestate.GameState;
 import client.gamestate.IsNotTurnState;
@@ -26,12 +27,12 @@ public class DiscardController extends Controller implements IDiscardController,
 	 * @param view View displayed to let the user select cards to discard
 	 * @param waitView View displayed to notify the user that they are waiting for other players to discard
 	 */
-	public DiscardController(IDiscardView view, IWaitView waitView, GameManager gameManager) {
+	public DiscardController(IDiscardView view, IWaitView waitView, GameManager gameManager, Facade facade) {
 
 		super(view);
 
 		this.waitView = waitView;
-		this.currState = new IsNotTurnState();
+		this.currState = new IsNotTurnState(facade);
 		gameManager.addObserver(this);
 	}
 

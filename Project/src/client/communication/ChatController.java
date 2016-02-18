77eec9369cb2ad934303adf8a064a9fd8f9dcf9d
bbarney7 <brennen.barney@gmail.com>
2016-02-17 19:42:28 -0700

    chat works

--- Project/src/client/communication/ChatController.java
+++ Project/src/client/communication/ChatController.java
@@ -1,10 +1,17 @@
 package client.communication;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 import client.base.*;
 import client.controller.Facade;
+import shared.definitions.CatanColor;
+import shared.definitions.GameModel;
+import shared.definitions.MessageLine;
+import shared.definitions.MessageList;
+import shared.definitions.Player;
 
 
 /**
@@ -12,11 +19,13 @@ import client.controller.Facade;
  */
 public class ChatController extends Controller implements IChatController, Observer {
 
+	private Facade facade;
 	public ChatController(IChatView view, Facade facade) {
 		
 		super(view);
+		this.facade = facade;
+		this.facade.addObserver(this);
 		
-		facade.addObserver(this);
 		
 	}
 
@@ -27,12 +36,32 @@ public class ChatController extends Controller implements IChatController, Obser
 
 	@Override
 	public void sendMessage(String message) {
-		
+		facade.sendChat(message);
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+		GameModel model = (GameModel)arg;
+		MessageList messages = model.getChat();
+		MessageLine[] lines = messages.getLines();
+		List<LogEntry> entries = new ArrayList();
+		Player[] allPlayers = model.getPlayers();
+		for(int i =0; i<lines.length; i++){
+			MessageLine line = lines[i];
+			String message = line.getMessage();
+			String source = line.getSource();
+			for(int j=0; j<allPlayers.length; j++){
+				Player player = allPlayers[j];
+				String name = player.getName();
+				if(name.equals(source)){
+					CatanColor color = player.getColor();
+					LogEntry entry = new LogEntry(color,message);
+					entries.add(entry);
+				}
+			}	
+		}
+		
+		this.getView().setEntries(entries);
 		
 	}
 

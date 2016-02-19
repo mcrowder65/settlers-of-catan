ac77b1e2814ae6bd52c0119d1505f5a6a2b9e2cc
menyx02 <meny_marquez@hotmail.com>
2016-02-19 14:53:41 -0700

    Turn tracker updated. Players, points, etc are displayed correctly

--- Project/src/client/turntracker/TurnTrackerView.java
+++ Project/src/client/turntracker/TurnTrackerView.java
@@ -79,7 +79,6 @@ public class TurnTrackerView extends PanelView implements ITurnTrackerView {
 	@Override
 	public void initializePlayer(int playerIndex, String playerName,
 			CatanColor playerColor) {
-				
 		playerPanel[playerIndex].setLayout(new BorderLayout());
 		
 		JLabel name = new JLabel(playerName);

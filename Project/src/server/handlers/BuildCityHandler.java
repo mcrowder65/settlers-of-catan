aa0e1da8849f218810cea8545e9efeabc59d4bed
Matt Crowder <mcrowder65@gmail.com>
2016-03-09 12:31:22 -0700

    updating handler javadocs

--- Project/src/server/handlers/BuildCityHandler.java
+++ Project/src/server/handlers/BuildCityHandler.java
@@ -6,17 +6,26 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import server.facade.IMovesFacade;
-
+/**
+ * This class is a build city handler. It handles building cities.
+ * @author mcrowder65
+ *
+ */
 public class BuildCityHandler implements HttpHandler {
 
 	IMovesFacade facade;
 	
-	
+	/**
+	 * the specific handler for building cities.
+	 * @param facade
+	 */
 	public BuildCityHandler(IMovesFacade facade) {
 		this.facade = facade;
 	}
 	
-	
+	/**
+	 * Handles... overrides HttpHandler handle method
+	 */
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
 		// TODO Auto-generated method stub

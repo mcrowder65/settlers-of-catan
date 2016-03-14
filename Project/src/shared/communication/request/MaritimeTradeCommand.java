054268d7f5d8a06d8eac52532d95cc0f47070a47
bbarney7 <brennen.barney@gmail.com>
2016-03-14 16:25:19 -0600

    maritimeTrade mostly done

--- Project/src/shared/communication/request/MaritimeTradeCommand.java
+++ Project/src/shared/communication/request/MaritimeTradeCommand.java
@@ -2,9 +2,15 @@ package shared.communication.request;
 
 import com.sun.net.httpserver.HttpExchange;
 
+import server.Game;
+import server.util.ServerGameMap;
+import server.util.ServerGameModel;
+import server.util.ServerPlayer;
+import server.util.ServerTurnTracker;
 import shared.communication.response.GetModelResponse;
 import shared.definitions.MirrorResourceType;
 import shared.definitions.ResourceType;
+import shared.locations.EdgeLocation;
 /**
  * Maritime trade command, extends MoveCommand
  * @author mcrowder65
@@ -47,7 +53,50 @@ public class MaritimeTradeCommand extends MoveCommand {
 	 */
 	@Override
 	public GetModelResponse execute() {
-		return null;
+		int playerIndex=this.getPlayerIndex();			
+ 		int index =this.gameIDCookie;		
+ 		GetModelResponse response = new GetModelResponse();	
+ 		Game game = Game.instance();		
+ 		ServerGameModel model = game.getGameId(index);		
+ 		ServerGameMap map = model.getServerMap();		
+ 		ServerTurnTracker turnTracker = model.getServerTurnTracker();		
+ 		ServerPlayer player = model.getServerPlayers()[playerIndex];		
+ 		int ratio = this.ratio;		
+ 		ResourceType input = this.getInput();
+ 		ResourceType output = this.getOutput();
+ 		
+		//making sure its the players turn		
+		if(checkTurn(turnTracker,playerIndex) == false){
+			response.setSuccess(false);
+			response.setErrorMessage("Wrong turn");
+			return response; //Need to throw some error here		
+		}
+		
+		String status = turnTracker.getStatus();		
+		//making sure its the right status		
+		if(!status.equals("Playing")){
+			response.setSuccess(false);
+			response.setErrorMessage("Wrong status");
+			return response; //Need to throw some error here		
+		}	
+		
+		if(player.getResources().hasResource(output) == false){
+			response.setSuccess(false);
+			response.setErrorMessage("Doesnt have resource");
+			return response;
+		}
+		
+		if(model.getBank().hasResource(input) == false){
+			response.setSuccess(false);
+			response.setErrorMessage("Bank is all out of that resource");
+			return response;
+		}
+		
+		player.addResource(input);
+		model.getBank().removeResource(output,ratio);
+		
+		response.setSuccess(true);
+		return response;
 	}
 
 

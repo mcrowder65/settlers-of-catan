3ea0d37a17a83b51868299ce760f078bbda00392
bbarney7 <brennen.barney@gmail.com>
2016-03-15 10:41:59 -0600

    discardCards execute done

--- Project/src/shared/communication/request/DiscardCardsCommand.java
+++ Project/src/shared/communication/request/DiscardCardsCommand.java
@@ -2,8 +2,14 @@ package shared.communication.request;
 
 import com.sun.net.httpserver.HttpExchange;
 
+import server.Game;
+import server.util.ServerGameMap;
+import server.util.ServerGameModel;
+import server.util.ServerPlayer;
+import server.util.ServerTurnTracker;
 import shared.communication.response.GetModelResponse;
 import shared.definitions.ResourceList;
+import shared.locations.VertexLocation;
 /**
  * This class executes the Discard Cards command,
  * extends MoveCommand
@@ -29,7 +35,33 @@ public class DiscardCardsCommand extends MoveCommand {
 	 */
 	@Override
 	public GetModelResponse execute() {
-		return null;
+		int gameIndex = this.gameIDCookie;
+		int playerIndex = this.getPlayerIndex();	
+		ResourceList cards = this.getDiscardedCards();	
+ 		Game game = Game.instance();	
+ 		GetModelResponse response = new GetModelResponse();
+ 		ServerGameModel model = game.getGameId(gameIndex);		
+ 		ServerGameMap map = model.getServerMap();		
+ 		ServerTurnTracker turnTracker = model.getServerTurnTracker();		
+ 		ServerPlayer player = model.getServerPlayers()[playerIndex];
+ 		String status = turnTracker.getStatus();
+ 		
+ 		if(player.getNumOfCards()<7){
+ 			response.setSuccess(false);
+			response.setErrorMessage("Not enough cards to discard");
+			return response;
+ 		}
+ 		
+ 		if(!status.equals("Discarding")){
+ 			response.setSuccess(false);
+			response.setErrorMessage("Wrong status");
+			return response;
+ 		}
+ 		
+ 		player.discardCards(cards);
+ 		response.setSuccess(true);
+		return response;
+
 	}
 
 	ResourceList discardedCards;

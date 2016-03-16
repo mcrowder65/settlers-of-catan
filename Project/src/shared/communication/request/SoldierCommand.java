afd543c38a457546c564c84d4c0377b275b56ef2
bbarney7 <brennen.barney@gmail.com>
2016-03-16 13:53:56 -0600

    soldier command re done

--- Project/src/shared/communication/request/SoldierCommand.java
+++ Project/src/shared/communication/request/SoldierCommand.java
@@ -2,7 +2,10 @@ package shared.communication.request;
 
 import com.sun.net.httpserver.HttpExchange;
 
+import server.Game;
+import server.util.*;
 import shared.communication.response.GetModelResponse;
+import shared.definitions.ResourceType;
 import shared.locations.HexLocation;
 /**
  * This class executes the soldier moves. Extends MoveCommand
@@ -34,7 +37,61 @@ public class SoldierCommand extends MoveCommand {
 	 */
 	@Override
 	public GetModelResponse execute() {
-		return null;
+		int gameIndex = this.gameIDCookie;
+		int playerIndex = this.getPlayerIndex();	
+		HexLocation robberLoc = this.getLocation();		
+ 		Game game = Game.instance();	
+ 		GetModelResponse response = new GetModelResponse();
+ 		ServerGameModel model = game.getGameId(gameIndex);		
+ 		ServerGameMap map = model.getServerMap();		
+ 		ServerTurnTracker turnTracker = model.getServerTurnTracker();		
+ 		ServerPlayer player = model.getServerPlayers()[playerIndex];
+ 		ServerPlayer victim = model.getServerPlayers()[victimIndex];
+ 		String status = turnTracker.getStatus();
+ 		
+ 		if(checkTurn(turnTracker,playerIndex) == false){		
+			response.setSuccess(false);
+			response.setErrorMessage("Wrong turn");
+			return response; 		
+		}
+ 		
+ 		if(!status.equals("Playing")){
+ 			response.setSuccess(false);
+			response.setErrorMessage("Wrong status");
+			return response;
+ 		}
+ 		
+ 		if(player.getOldDevCards().getSoldier() < 1){
+ 			response.setSuccess(false);
+			response.setErrorMessage("No dev card to play");
+			return response;
+ 		}
+ 		
+ 		if(map.isLand(robberLoc) == false){
+ 			response.setSuccess(false);
+			response.setErrorMessage("Invalid Hex Location");
+			return response;
+ 		}
+ 		
+ 		if(victim.getResources().isEmpty()){
+			response.setSuccess(true);
+			return response; 
+		}
+ 		
+ 		ResourceType resource = model.generateRandomResource();
+		
+		while(victim.getResources().hasResource(resource) == false){
+			resource = model.generateRandomResource();
+		}
+		
+		victim.removeResource(resource);
+		player.addResource(resource);
+		response.setSuccess(true);
+		map.setRobber(robberLoc);
+		player.playSoldierCard();
+		return response; 
+
+	
 	}
 
 	public HexLocation getLocation() {

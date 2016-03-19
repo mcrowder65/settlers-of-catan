4af8e65dfceb0a41b7d14a0cb9f371b8496d3e9f
bbarney7 <brennen.barney@gmail.com>
2016-03-19 13:08:23 -0600

    offer trade handler added

---
+++ Project/src/server/handlers/OfferTradeHandler.java
@@ -0,0 +1,43 @@
+package server.handlers;
+
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import server.facade.IMovesFacade;
+import shared.communication.response.Response;
+import sun.net.www.protocol.http.HttpURLConnection;
+
+public class OfferTradeHandler implements HttpHandler  {
+	IMovesFacade facade;
+	
+	
+	/**
+	 * Constructor
+	 * @param facade IMovesFacade type
+	 */
+	public OfferTradeHandler(IMovesFacade facade) {
+		this.facade = facade;
+	}
+	
+	/**
+	 * Handles... overrides HttpHandler handle method
+	 */
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		Response response = null;
+		try{
+		   response = facade.offerTrade(exchange);
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		if (response.getCookie() != null)
+			exchange.getResponseHeaders().add(response.getCookie().getKey(), response.getCookie().getValue());
+		exchange.getResponseHeaders().add("Content-type", "application/json");
+		exchange.sendResponseHeaders(response.isSuccess() ? HttpURLConnection.HTTP_OK : HttpURLConnection.HTTP_BAD_REQUEST, 0);
+		exchange.getResponseBody().write(response.toString().getBytes());
+		exchange.getResponseBody().close();
+	}
+}

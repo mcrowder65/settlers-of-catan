f84da2f017177e868a579b1343b3525174146b8d
Matt Crowder <mcrowder65@gmail.com>
2016-03-30 14:19:24 -0600

    got rid of sequel pro comments

--- 340server.sql
+++ 340server.sql
@@ -1,27 +1,3 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4529
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: localhost (MySQL 5.5.42)
-# Database: 340server
-# Generation Time: 2016-03-30 20:15:39 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Dump of table commands
-# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `commands`;
 
@@ -34,11 +10,6 @@ CREATE TABLE `commands` (
   CONSTRAINT `commands to game` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-
-# Dump of table games
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `games`;
 
 CREATE TABLE `games` (
@@ -47,11 +18,6 @@ CREATE TABLE `games` (
   `title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
-# Dump of table join
-# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `join`;
 
@@ -67,11 +33,6 @@ CREATE TABLE `join` (
   CONSTRAINT `join to game` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-
-# Dump of table users
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
@@ -81,12 +42,3 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

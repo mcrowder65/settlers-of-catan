793bb0821c2bfe1876845c3a898c554408a260d5
menyx02 <meny_marquez@hotmail.com>
2016-04-06 12:11:15 -0600

    Wipeout execution server

--- Project/src/server/Server.java
+++ Project/src/server/Server.java
@@ -28,7 +28,7 @@ public class Server {
 	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
 		new Server().run(args);
 	}
-	
+
 	private AcceptTradeHandler acceptTradeHandler;
 	private AddAIHandler addAIHandler;
 	private BuildCityHandler buildCityHandler;
@@ -55,19 +55,37 @@ public class Server {
 	private YearOfPlentyHandler yearOfPlentyHandler;
 	private OfferTradeHandler offerTradeHandler;
 	public void run(String[] args) throws SAXException, IOException, ParserConfigurationException {
-		
+
+		//Wipe out persistence data
+		if(args.length == 1){
+			if(!args[0].equals("Wipeout")){
+				System.out.println("Usage: ant our-server <Wipeout>");
+				return;
+			}
+		}
+
 		if (args.length != 2) {
 			System.out.println("Usage: ant our-server <persistence-type> <commands-between-checkpoints>");
 			return;
 		}
 		String persistenceIdentifier = args[0];
 		int commandsBetweenCheckpoints = Integer.parseInt(args[1]);
-		
-		
+
+
 		List<PluginDefinition> definitions = ConfigurationTool.readConfigFile("plugin.xml");
 		ClassLoaderTool.loadPlugins("resources");
-	
-	
+
+		//Wipe out persistence data
+		if(args.length == 1) {
+			for (PluginDefinition def : definitions) {
+				Game.instance().initPersistanceProvider(1, def.path);
+				Game.instance().getPersistenceProvider().dropTables();
+			}
+			return;
+		}
+
+
+
 		for (PluginDefinition def : definitions) {
 			if (def.name.equals(persistenceIdentifier)) {
 				Game.instance().initPersistanceProvider(commandsBetweenCheckpoints, def.path);
@@ -79,37 +97,37 @@ public class Server {
 			return;
 		}
 		Game.instance().restorePersistenceData();
-	     
-		
-		
+
+
+
 		int port = 8081;
-		
+
 		System.out.println("Port =" + port);
-		
-		
+
+
 		try {
 			httpServer = HttpServer.create(new InetSocketAddress(port), MAX_WAITING_CONNECTIONS);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
-		
+
 		IMovesFacade movesFacade = new ServerMovesFacade();
 		IInnerGameFacade innerGameFacade = new ServerInnerGameFacade();
 		IOuterGameFacade outerGameFacade = new ServerOuterGameFacade();
 		IUserFacade  userFacade = new ServerUserFacade();
-		
+
 		loginHandler = new LoginHandler(userFacade);
 		registerHandler = new RegisterHandler(userFacade);
-		
+
 		createGameHandler = new CreateGameHandler(outerGameFacade);
 		listGamesHandler = new ListGamesHandler(outerGameFacade);
 		joinGameHandler  = new JoinGameHandler(outerGameFacade);
-		
+
 		getModelHandler = new GetModelHandler(innerGameFacade);
 		listAIHandler = new ListAIHandler(innerGameFacade);
 		addAIHandler = new AddAIHandler(innerGameFacade);
-		
+
 		acceptTradeHandler = new AcceptTradeHandler(movesFacade);
 		buildCityHandler = new BuildCityHandler(movesFacade);
 		buildSettlementHandler = new BuildSettlementHandler(movesFacade);
@@ -127,24 +145,24 @@ public class Server {
 		soldierHandler = new SoldierHandler(movesFacade);
 		yearOfPlentyHandler = new YearOfPlentyHandler(movesFacade);
 		offerTradeHandler = new OfferTradeHandler(movesFacade);
-		
+
 		httpServer.setExecutor(null);
-		
+
 		httpServer.createContext("/docs/api/data", new JSONAppender("") );
 		httpServer.createContext("/docs/api/view", new BasicFile("") );
-		
-		
+
+
 		httpServer.createContext("/user/login", loginHandler);
 		httpServer.createContext("/user/register", registerHandler);
-		
+
 		httpServer.createContext("/games/list", listGamesHandler);
 		httpServer.createContext("/games/create", createGameHandler);
 		httpServer.createContext("/games/join", joinGameHandler);
-		
+
 		httpServer.createContext("/game/model", getModelHandler);
 		httpServer.createContext("/game/listAI", listAIHandler);
 		httpServer.createContext("/game/addAI", addAIHandler);
-		
+
 		httpServer.createContext("/moves/offerTrade", offerTradeHandler);
 		httpServer.createContext("/moves/acceptTrade", acceptTradeHandler);
 		httpServer.createContext("/moves/buildCity", buildCityHandler);
@@ -162,12 +180,12 @@ public class Server {
 		httpServer.createContext("/moves/sendChat", sendChatHandler);
 		httpServer.createContext("/moves/Soldier", soldierHandler);
 		httpServer.createContext("/moves/Year_Of_Plenty", yearOfPlentyHandler);
-		
 
-		
-		
+
+
+
 		httpServer.start();
-		
-		
+
+
 	}
 }

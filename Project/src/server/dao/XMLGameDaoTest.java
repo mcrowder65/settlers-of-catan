742bcc43747d5263cb135e1f85e5979e779a3cf2
Eric <soreric94@gmail.com>
2016-04-06 12:44:49 -0600

    resetPersistence

--- Project/src/server/dao/XMLGameDaoTest.java
+++ Project/src/server/dao/XMLGameDaoTest.java
@@ -66,7 +66,7 @@ public class XMLGameDaoTest {
 	
 	public void deleteAll() throws IOException {
 		XMLGameDAO dao = new XMLGameDAO();
-		dao.dropTables();
+		dao.resetPersistence();
 	}
 	
 	public void addGameTest() throws IOException {

920b5817aaaf8a1c9715e3530a82dcdb129ca10c
menyx02 <meny_marquez@hotmail.com>
2016-04-07 23:30:24 -0600

    Ais storing commands

--- Project/src/server/facade/ServerAIFacade.java
+++ Project/src/server/facade/ServerAIFacade.java
@@ -1,5 +1,6 @@
 package server.facade;
 
+import server.Game;
 import shared.communication.request.*;
 import shared.communication.response.GetModelResponse;
 import shared.definitions.ResourceList;
@@ -16,12 +17,15 @@ public class ServerAIFacade implements IAIFacade {
 		this.playerIndex = playerIndex;
 		this.gameId = gameId;
 	}
-	
+
 	@Override
 	public void rollNumber(int number) {
 		RollNumberCommand command = new RollNumberCommand(playerIndex, number);
 		command.setGameCookie(gameId);
-		command.execute();
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 
 	@Override
@@ -29,110 +33,142 @@ public class ServerAIFacade implements IAIFacade {
 		DiscardCardsCommand command = new DiscardCardsCommand(playerIndex, resources);
 		command.setGameCookie(gameId);
 		command.doSuppressAIHandling();
-		command.execute();
-		
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 
 	@Override
 	public void buildRoad(EdgeLocation location, boolean free) {
-		 BuildRoadCommand command = new BuildRoadCommand(playerIndex, free, location);
-		 command.setGameCookie(gameId);
-		 command.execute();
+		BuildRoadCommand command = new BuildRoadCommand(playerIndex, free, location);
+		command.setGameCookie(gameId);
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 
 	@Override
 	public boolean buildSettlement(VertexLocation location, boolean free) {
 		BuildSettlementCommand command = new BuildSettlementCommand(playerIndex, free, location);
-		 command.setGameCookie(gameId);
+		command.setGameCookie(gameId);
 		GetModelResponse response = command.execute();
-		 return response.isSuccess();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
+		return response.isSuccess();
 	}
 
 	@Override
 	public void buildCity(VertexLocation location) {
 		BuildCityCommand command = new BuildCityCommand(playerIndex, location);
-		 command.setGameCookie(gameId);
-		 command.execute();
-		 
-		
+		command.setGameCookie(gameId);
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 
 	@Override
 	public void buyDevCard() {
 		BuyDevCardCommand command = new BuyDevCardCommand(playerIndex);
-		 command.setGameCookie(gameId);
-		 command.execute();
-		 
+		command.setGameCookie(gameId);
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 
 	@Override
 	public void monopoly(ResourceType resource) {
 		MonopolyCommand command = new MonopolyCommand(playerIndex, resource);	
-		 command.setGameCookie(gameId);
-		 command.execute();
-		
+		command.setGameCookie(gameId);
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 
 	@Override
 	public void soldier(int victimIndex, HexLocation location) {
 		SoldierCommand command = new SoldierCommand(playerIndex, location, victimIndex);
-		 command.setGameCookie(gameId);
-		 command.execute();
+		command.setGameCookie(gameId);
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 
 	@Override
 	public void monument() {
 		MonumentCommand command = new MonumentCommand(playerIndex);
-		 command.setGameCookie(gameId);
-		 command.execute();
+		command.setGameCookie(gameId);
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 
 	@Override
 	public void yearOfPlenty(ResourceType resource1, ResourceType resource2) {
 		YearOfPlentyCommand command = new YearOfPlentyCommand(playerIndex, resource1, resource2);
-		 command.setGameCookie(gameId);
-		 command.execute();
-		 
+		command.setGameCookie(gameId);
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 
 	@Override
 	public void roadBuilding(EdgeLocation spot1, EdgeLocation spot2) {
 		RoadBuildingCommand command = new RoadBuildingCommand(playerIndex, spot1, spot2);
-		 command.setGameCookie(gameId);
-		 command.execute();
-		 
+		command.setGameCookie(gameId);
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 
 	@Override
 	public void robPlayer(int victimIndex, HexLocation location) {
 		RobPlayerCommand command = new RobPlayerCommand(playerIndex, location, victimIndex);
-		 command.setGameCookie(gameId);
-		 command.execute();
-		 
+		command.setGameCookie(gameId);
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 
 	@Override
 	public void acceptTrade(boolean willAccept) {
 		AcceptTradeCommand command = new AcceptTradeCommand(playerIndex, willAccept);
-		 command.setGameCookie(gameId);
-		 command.execute();
+		command.setGameCookie(gameId);
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 
 	@Override
 	public void maritimeTrade(int ratio, ResourceType input, ResourceType output) {
-		 MaritimeTradeCommand command = new MaritimeTradeCommand(playerIndex, ratio, input, output);
-		 command.setGameCookie(gameId);
-		 command.execute();
-		 
+		MaritimeTradeCommand command = new MaritimeTradeCommand(playerIndex, ratio, input, output);
+		command.setGameCookie(gameId);
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
 	@Override
 	public void finishTurn(){
 		FinishTurnCommand command = new FinishTurnCommand(playerIndex);
-		 command.setGameCookie(gameId);
-		 command.execute();
-		 
+		command.setGameCookie(gameId);
+		GetModelResponse response = command.execute();
+		if(response.isSuccess()) {
+			Game.instance().getProvider().addCommand(command);
+		}
 	}
-	
+
 	@Override
 	public int getGameId() {
 		return gameId; 

c0d5921ca6715c465c436703697045ae2bba54fa
bbarney7 <brennen.barney@gmail.com>
2016-04-11 16:37:18 -0600

    another one

--- Project/src/server/dao/CommandParams.java
+++ Project/src/server/dao/CommandParams.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import shared.communication.request.MoveCommand;
-
+/**
+ * wrapper class to wite out all commands to a single file
+ * @author Brennen
+ *
+ */
 public class CommandParams {
 
-	public List<MoveCommand> allCommands = new ArrayList<MoveCommand>();
-	public int gameID;
+	public List<MoveCommand> allCommands = new ArrayList<MoveCommand>(); //list to hold all the commands
+	public int gameID; //game id for those commands 
 }
--- Project/src/server/dao/GameParams.java
+++ Project/src/server/dao/GameParams.java
@@ -1,42 +1,74 @@
 package server.dao;
 
 import server.util.ServerGameModel;
-
+/**
+ * wrapper class to hold all the of the info needed when storing a game
+ * @author Brennen
+ *
+ */
 public class GameParams {
 
-	private int id;
-	private ServerGameModel model;
-	private String title;
-	
+	private int id; //game id
+	private ServerGameModel model; 
+	private String title; //title of game
+	 
+	/**
+	 * constuctor for GameParms
+	 * @param id
+	 * @param model
+	 * @param title
+	 */
 	public GameParams(int id, ServerGameModel model, String title){
 		this.id = id;
 		this.model = model;
 		this.title = title;
 	}
 
+	/**
+	 * gets id
+	 * @return id
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * gets gameModel
+	 * @return ServerGameModel
+	 */
 	public ServerGameModel getModel() {
 		return model;
 	}
 
+	/**
+	 * gets the title of the game
+	 * @return title
+	 */
 	public String getTitle() {
 		return title;
 	}
 
+	/**
+	 * sets the game id
+	 * @param id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * sets the model
+	 * @param model
+	 */
 	public void setModel(ServerGameModel model) {
 		this.model = model;
 	}
 
+	/**
+	 * sets the title
+	 * @param title
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
-	
 }
